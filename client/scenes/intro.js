23c1a301083392ffea93145262daf5adf2f7b7c3
Saarthak Seth <saarthakseth@gmail.com>
2021-10-31 00:58:31 +0530

    implemented collision of cue with white ball

--- client/scenes/intro.js
+++ client/scenes/intro.js
@@ -87,10 +87,15 @@ export default class Scene_8BallPool extends Phaser.Scene {
         ball.setBounce(0.9);
         ball.setFriction(0, 0.008, 0.1);
         if (key === 'ball_16') {
-            ball.setVelocity(20, 0);
-            ball.setAngularVelocity(0)
+            // ball.setVelocity(50, 0);
+            // ball.setAngularVelocity(0)
             this.cueBall = ball
-        } else ball.setVelocity(0, 0);
+            ball.setCollisionCategory(this.cueBallCategory)
+        } else {
+            ball.setCollisionCategory(this.ballCategory)
+        }
+        this.balls.push(ball)
+
         // ball.setCollisionCategory(this.ballCategory)
         // ball.setCollidesWith(this.ballCategory)
         // let posv = ball.body.position
@@ -122,6 +127,10 @@ export default class Scene_8BallPool extends Phaser.Scene {
         board.displayWidth = this.sys.canvas.width
         board.displayHeight = this.sys.canvas.height
 
+        this.ballCategory = this.matter.world.nextCategory();
+        this.cueCategory = this.matter.world.nextCategory();
+        this.cueBallCategory = this.matter.world.nextCategory();
+        this.cushionCategory = this.matter.world.nextCategory();
 
         // x: 95 y: 90
         // width: 1355 height: 600
@@ -139,6 +148,7 @@ export default class Scene_8BallPool extends Phaser.Scene {
         cushion1.setFrictionStatic(0.1)
         cushion1.displayHeight = 65
         cushion1.displayWidth = 540
+        cushion1.setCollisionCategory(this.cushionCategory)
         let cushion2 = this.matter.add.image(1045, 55, 'platform');
         cushion2.setBody({
             type: 'trapezoid',
@@ -150,6 +160,7 @@ export default class Scene_8BallPool extends Phaser.Scene {
         cushion2.setFrictionStatic(0.1)
         cushion2.displayHeight = 65
         cushion2.displayWidth = 540
+        cushion2.setCollisionCategory(this.cushionCategory)
         let cushion3 = this.matter.add.image(1380, 390, 'platform');
         cushion3.setBody({
             type: 'trapezoid',
@@ -162,6 +173,7 @@ export default class Scene_8BallPool extends Phaser.Scene {
         cushion3.setFrictionStatic(0.1)
         cushion3.displayHeight = 505
         cushion3.displayWidth = 60
+        cushion3.setCollisionCategory(this.cushionCategory)
         let cushion4 = this.matter.add.image(65, 390, 'platform');
         cushion4.setBody({
             type: 'trapezoid',
@@ -174,6 +186,7 @@ export default class Scene_8BallPool extends Phaser.Scene {
         cushion4.setFrictionStatic(0.1)
         cushion4.displayHeight = 615
         cushion4.displayWidth = 60
+        cushion4.setCollisionCategory(this.cushionCategory)
         let cushion5 = this.matter.add.image(410, 725, 'platform');
         cushion5.setBody({
             type: 'trapezoid',
@@ -185,6 +198,7 @@ export default class Scene_8BallPool extends Phaser.Scene {
         cushion5.setFrictionStatic(0.1)
         cushion5.displayHeight = 65
         cushion5.displayWidth = 595
+        cushion5.setCollisionCategory(this.cushionCategory)
         let cushion6 = this.matter.add.image(1045, 725, 'platform');
         cushion6.setBody({
             type: 'trapezoid',
@@ -196,31 +210,65 @@ export default class Scene_8BallPool extends Phaser.Scene {
         cushion6.setFrictionStatic(0.1)
         cushion6.displayHeight = 65
         cushion6.displayWidth = 595
+        cushion6.setCollisionCategory(this.cushionCategory)
 
+        this.balls = []
         this.createBalls()
-
-        this.cursors = this.input.keyboard.createCursorKeys();
-        // this.ballCategory = this.matter.world.nextCategory();
-        // this.cueCategory = this.matter.world.nextCategory();
 
         this.cue = this.matter.add.sprite(250, 370, 'cue')
         this.cue.setBody({
             type: 'trapezoid',
             slope: 0.5
         })
+        this.cue.setBounce(1)
+        this.cue.setMass(10)
         this.cue.setRotation(Math.PI / 2)
-        this.cue.setCollidesWith([])
+        this.cue.setCollisionCategory(this.cueCategory)
+        this.cue.setCollidesWith([this.cueBallCategory])
+        let categories = [this.ballCategory, this.cueBallCategory, this.cushionCategory]
+
+        cushion1.setCollidesWith(categories)
+        cushion2.setCollidesWith(categories)
+        cushion3.setCollidesWith(categories)
+        cushion4.setCollidesWith(categories)
+        cushion5.setCollidesWith(categories)
+        cushion6.setCollidesWith(categories)
+        this.balls.forEach(ball => {
+            ball.setCollidesWith(categories)
+        })
+        categories.push(this.cueCategory)
+        this.cueBall.setCollidesWith(categories)
+
+        this.matter.body.setPosition(this.cue.body, this.matter.vector.create(this.cueBall.body.position.x - 420, this.cueBall.body.position.y))
+
+        this.cursors = this.input.keyboard.createCursorKeys();
 
     }
 
     update() {
         let ballPosition = this.cueBall.body.position
-        if (Math.abs(this.cueBall.body.velocity.x) > 1e-2 || Math.abs(this.cueBall.body.velocity.y) > 1e-2) {
+        let moveCue = false
+
+        this.balls.forEach(ball => {
+            if (Math.abs(ball.body.velocity.x) > 1e-2 || Math.abs(ball.body.velocity.y) > 1e-2) {
+                moveCue = true
+            }
+        })
+        if (moveCue) {
             this.cue.setVisible(false)
             this.matter.body.setPosition(this.cue.body, this.matter.vector.create(ballPosition.x - 420, ballPosition.y))
-        } else this.cue.setVisible(true)
-
-        if (this.cursors.left.isDown) this.matter.body.rotate(this.cue.body, -Math.PI / 180, this.matter.vector.create(ballPosition.x, ballPosition.y))
-        else if (this.cursors.right.isDown) this.matter.body.rotate(this.cue.body, Math.PI / 180, this.matter.vector.create(ballPosition.x, ballPosition.y))
+        } else {
+            let cuePosition = this.cue.body.position
+            let velocityVector = new Phaser.Math.Vector2(ballPosition.x - cuePosition.x, ballPosition.y - cuePosition.y)
+            let angle = velocityVector.angle() + Math.PI / 2
+            this.cue.setRotation(angle)
+            this.cue.setVisible(true)
+            if (this.cursors.left.isDown) this.matter.body.rotate(this.cue.body, -Math.PI / 180, this.matter.vector.create(ballPosition.x, ballPosition.y))
+            else if (this.cursors.right.isDown) this.matter.body.rotate(this.cue.body, Math.PI / 180, this.matter.vector.create(ballPosition.x, ballPosition.y))
+            else if (this.cursors.down.isDown) {
+                console.log("power increasing")
+                this.matter.applyForceFromAngle(this.cue.body, 1, angle - Math.PI / 2)
+            }
+        }
     }
 }
